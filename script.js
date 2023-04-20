15eae392ce1feb7116d022f50b91547b576a3ebe
*Aleksey <*atsukanov89@gmail.com*>
2023-04-20 21:51:46 +0300

    Выполнено задание №1

---
+++ script.js
@@ -0,0 +1,18 @@
+//  Задание № 1
+
+function minNum (a, b) {
+    if (a < b) {
+        return a
+    } else {
+        return b
+    }
+}
+console.log (minNum(2, 5));
+
+// Задание №1 (тернальный оператор)
+
+function minNumber(a, b) {
+    return a < b ? a : b 
+
+}
+console.log(minNumber(2, 13));
